d0d7f329f73ca12b52ab8355b25902b8a795a120
mahadghauri <233523@students.au.edu.pk>
2024-10-16 19:51:02 +0500

    files

---
+++ Lab 4/ConsoleApp1/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Person
+{
+    protected string name;
+    public Person() 
+    {
+        name = null;
+    }
+}
+
+public class Student : Person
+{
+    private int id;
+    private int age;
+    private string dep;
+    public Student()
+    {
+        id = 0;
+        age = 0;
+        dep = null;
+    }
+
+    public void setid(int id)
+    {
+        this.id = id;
+    }
+
+    public void setname(string name)
+    {
+        this.name = name;
+    }
+
+    public void setage(int age)
+    {
+        this.age = age;
+    }
+
+    public void setdep(string dep)
+    {
+        this.dep = dep;
+    }
+
+    public void display()
+    {
+        Console.WriteLine("Name = " + name);
+        Console.WriteLine("ID = " + id);
+        Console.WriteLine("Age = " + age);
+        Console.WriteLine("Department = " + dep);
+    }
+}
+
---
+++ Lab 4/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Question1
+{
+    static void Main()
+    {
+        Student st = new Student();
+        st.setname("Mahad Ghauri");
+        st.setid(233523);
+        st.setage(20);
+        st.setdep("BSCS");
+        st.display();
+    }
+}
